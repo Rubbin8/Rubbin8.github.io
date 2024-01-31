9e6fa3154b32803cd357a9ef3383ca7b43306fee
Rubbin8 <robinknutsen8@gmail.com>
2024-01-30 23:36:30 -0800

    Added diamonds when you click on the numbers page

--- js/date.js
+++ js/date.js
@@ -1,17 +1,52 @@
-var today = new Date();
-if(today.getYear() < 2025)// && today.getMonth() < 2 && today.getDate() < 19)
+/*const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+
+function rolld20(var x, var y)
 {
-	if(today.getMonth() < 1)
-	{
-		if(today.getDate() < 19)
-		{
-			document.write('<p>The next meeting is on January 19th at 4PM, don\'t miss it!</p>');
-		}
-	}
-	else{
-	document.write('<p>It\'s too late for the meeting on January 19, 2024. Better luck next time!</p>');	
-	}
+		ctx.fillStyle = "#9900FF";
+		ctx.fillRect(0, 0, 150, 75);
+
+}*/
+
+document.write('<h3>Greetings, traveler!</h3>');
+var canvas = document.getElementById("myCanvas");
+var c = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+var mouseX = 0;
+var mouseY = 0;
+
+
+canvas.addEventListener("mousemove", moveMouse);
+function moveMouse(event)
+{
+	mouseX = event.x - 5;
+	mouseY = event.y - 420;
+	
 }
-else{
-	document.write('<p>It\'s too late for the meeting on January 19, 2024. Better luck next time!</p>');	
+
+
+function mouseclick(event)
+{
+
+	"use Strict";
+	W = canvas.width;
+	H = canvas.height;
+	 c.beginPath();
+           c.fillStyle = "#9900FF";
+            c.lineWidth = 2;
+            c.strokeStyle = "#CC22FF";
+			c.moveTo(mouseX, mouseY);
+			c.lineTo(mouseX + 10, mouseY + 10);
+			c.lineTo(mouseX, mouseY + 20);
+			c.lineTo(mouseX - 10, mouseY + 10);
+			c.lineTo(mouseX, mouseY);
+			//c.lineTo(W/2, H/2);
+			c.closePath();
+			c.fill();
+            c.stroke();
 }
+canvas.addEventListener("click", mouseclick, false);
